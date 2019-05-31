b2696a9950fd331aacd3416dffb251c53993d2c4
Curro Rodriguez <s1650043@ed.ac.uk>
2019-05-31 11:58:03 +0200

    Adding the example analysis codes.

---
+++  quench_rejuvenation_3_4_2.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 24 18:56:07 2018
+
+@author: currorodriguez
+"""
+
+# Import required libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+sys.path.insert(0,'/Users/currorodriguez/Documents/Astrophysics_Year_4/SH_Project/quench_repository/Progen')
+sys.path.insert(0, '../Mergers')
+from import_progen import importApp
+from merger_finder_8 import myrunningmedian
+from quenchingFinder8_5_3 import GalaxyData, quenchingFinder2, rejuvenation_rate_calculator, quenching_histogram
+
+# Extract progen data from txt files
+d, ngal = importApp()
+print('Total number of galaxies at z = 0: '+str(ngal))
+
+#Store the galaxies sorted in objects of type GalaxyData
+galaxies = []
+for i in range(ngal):
+    ssfr_gal = (d['sfr_gal' + str(i)]/d['m_gal'+str(i)])[::-1]+1e-14
+    sfe_gal = d['sfe_gal' + str(i)][::-1]
+    z_gal = d['z_gal' + str(i)][::-1]
+    galaxy_t = d['galaxy_t' + str(i)][::-1]
+    galaxy_m = d['m_gal'+str(i)][::-1]
+    fgas_gal = d['fgas_gal'+str(i)][::-1]
+    gal_type = d['gal_type'+str(i)][::-1]
+    galaxy = GalaxyData(i, ssfr_gal, sfe_gal, z_gal, galaxy_t, galaxy_m, fgas_gal, gal_type)
+    galaxies.append(galaxy)
+
+max_ngal = len(galaxies)
+mass_limit = 9.5
+# Perform the quenching and rejuvenation analysis
+galaxies_interpolated = quenchingFinder2(galaxies[0:max_ngal], 1, mass_limit)
+
+# Save results of rejuvenations coming from first loop
+reju_z = []
+reju_m = []
+reju_t = []
+
+
+for i in range(len(galaxies)):
+    galaxy = galaxies[i]
+    for k in range(0, len(galaxy.rate), 3):
+        #if np.log10(galaxy.rate[k+1])>=mass_limit:
+        reju_z.append(galaxy.rate[k])
+        reju_t.append(galaxy.rate[k+1])
+        reju_m.append(np.log10(galaxy.rate[k+2]))
+
+rates, red_cent, rates_sig = rejuvenation_rate_calculator(d, reju_z, '../Progen/galaxy_count.txt')
+
+print('Total number of rejuvenations: '+str(len(reju_z)))
+print('Number of quenching events in first loop: '
+        +str(sum([1 for galaxy in galaxies[0:max_ngal] for quench in galaxy.quenching])))
+#Interpolation analysis
+print("Interpolating")
+
+quenchingFinder2(galaxies_interpolated, 1, mass_limit, True)
+
+redshifts2_all = []
+quenching_times2_all = []
+ste_mass2_all = []
+frac_gas2_all = []
+thubble2_all = []
+
+redshifts2 = [[[],[],[]],[[],[],[]]]
+quenching_times2 = [[[],[],[]],[[],[],[]]]
+ste_mass2 = [[[],[],[]],[[],[],[]]]
+frac_gas2 = [[[],[],[]],[[],[],[]]]
+thubble2 = [[[],[],[]],[[],[],[]]]
+sfr_2 = [[[],[],[]],[[],[],[]]]
+
+scatter_labels = [['Final quenching Sat', 'Non-final quenching Sat', 'Final quenching Sat with rejuvenation' ],['Final quenching Central', 'Non-final quenching Central', 'Final quenching Central with rejuvenation']]
+scatter_markers = ['.','*', '.']
+scatter_faces = [['#e90c35','#ec5b75','none'],['#0c86e4','#8d0ce4','none']]
+scatter_colours = [['#e90c35','#ec5b75','#e90c35'],['#0c86e4','#8d0ce4','#0c86e4']]
+finalis = 0
+nofinalis = 0
+for i in range(0, len(galaxies_interpolated)):
+    galaxy = galaxies_interpolated[i]
+    if len(galaxy.quenching)>0:
+        lastquench = galaxy.quenching[-1]
+        # if len(galaxy.quenching)>1:
+        #     for caca in range(0, len(galaxy.quenching)):
+        #         print(galaxy.quenching[caca].quench_time)
+    for quench in galaxy.quenching:
+        start = quench.above9 + 1
+        end = quench.below11
+        if quench is lastquench:
+            pos = 0
+            finalis = finalis + 1
+            for k in range(0, len(galaxy.rate), 3):
+                if 0.2*galaxy.galaxy_t[start]> (galaxy.galaxy_t[start] - galaxy.rate[k+1]) >=0:
+                    pos = 2
+        else:
+            nofinalis = nofinalis + 1
+            pos = 1
+        #print(len(galaxy.z_gal), start, end, galaxy.id)
+        if np.log10(galaxy.m_gal)>=mass_limit:
+            redshifts2[int(quench.type)][pos].append(galaxy.z_gal)
+            ste_mass2[int(quench.type)][pos].append(np.log10(galaxy.m_gal))
+            quenching_times2[int(quench.type)][pos].append(np.log10(quench.quench_time))
+            frac_gas2[int(quench.type)][pos].append(np.log10(galaxy.fgas_gal))
+            thubble2[int(quench.type)][pos].append(np.log10(galaxy.galaxy_t[end]))
+            sfr_2[int(quench.type)][pos].append(np.log10(galaxy.ssfr_gal[start-1]*galaxy.m_gal))
+            redshifts2_all.append(galaxy.z_gal)
+            ste_mass2_all.append(np.log10(galaxy.m_gal))
+            quenching_times2_all.append(np.log10(quench.quench_time/galaxy.galaxy_t[end]))
+            frac_gas2_all.append(np.log10(galaxy.fgas_gal))
+            thubble2_all.append(np.log10(galaxy.galaxy_t[end]))
+print(len(quenching_times2[0][0]), len(quenching_times2[0][1]), len(quenching_times2[0][2]), len(quenching_times2[0][0])+len(quenching_times2[0][1]))
+print(len(quenching_times2[1][0]), len(quenching_times2[1][1]), len(quenching_times2[1][2]), len(quenching_times2[1][0])+len(quenching_times2[1][1]))
+#print(galaxies_interpolated[0].quenching[-1])
+print('Number of quenching events in second loop: '
+        +str(sum([1 for galaxy in galaxies_interpolated for quench in galaxy.quenching])))
+
+# tau = np.linspace(0.01,0.1, 100)
+# sfr_tau = np.zeros(len(tau))
+# for tau_i in range(0, len(tau)):
+#     sfr_tau[tau_i] = 1/(80*0.0003*tau[tau_i])
+# Plot the results
+import seaborn as sns
+sns.set(style="white")
+fig = plt.figure(num=None, figsize=(8, 10), dpi=80, facecolor='w', edgecolor='k')
+figre = plt.figure(num=None, figsize=(7, 5), dpi=80, facecolor='w', edgecolor='k')
+# fig4 = plt.figure(num=None, figsize=(7, 5), dpi=80, facecolor='w', edgecolor='k')
+# fig5 = plt.figure(num=None, figsize=(7, 5), dpi=80, facecolor='w', edgecolor='k')
+# ax4 = fig4.add_subplot(1,1,1)
+# ax5 = fig5.add_subplot(1,1,1)
+ax1 = fig.add_subplot(2,1,1)
+ax2 = fig.add_subplot(2,1,2)
+axre = figre.add_subplot(1,1,1)
+# ax5.plot(np.log10(tau), np.log10(sfr_tau), 'k-')
+for i in range(0, len(scatter_labels)):
+    for j in range(0, len(scatter_markers)):
+        if j==2:
+            ax1.scatter(redshifts2[i][j], quenching_times2[i][j], edgecolor=scatter_colours[i][j], marker=scatter_markers[j], facecolors=scatter_faces[i][j], s=60)
+            ax2.scatter(ste_mass2[i][j], quenching_times2[i][j], edgecolor=scatter_colours[i][j], marker=scatter_markers[j], facecolors=scatter_faces[i][j], s=60)
+            axre.scatter(ste_mass2[i][j], quenching_times2[i][j], edgecolor=scatter_colours[i][j], marker=scatter_markers[j], facecolors=scatter_faces[i][j], s=60)
+        else:
+            ax1.scatter(redshifts2[i][j], quenching_times2[i][j], edgecolor=scatter_colours[i][j], label =scatter_labels[i][j], marker=scatter_markers[j], facecolors=scatter_faces[i][j], s=60)
+            ax2.scatter(ste_mass2[i][j], quenching_times2[i][j], edgecolor=scatter_colours[i][j], label =scatter_labels[i][j], marker=scatter_markers[j], facecolors=scatter_faces[i][j], s=60)
+            axre.scatter(ste_mass2[i][j], quenching_times2[i][j], edgecolor=scatter_colours[i][j], label =scatter_labels[i][j], marker=scatter_markers[j], facecolors=scatter_faces[i][j], s=60)
+        # if scatter_labels[i][j]=='Non-final quenching Sat' or scatter_labels[i][j]=='Non-final quenching Central':
+        #     print(quenching_times2[i][j])
+        #print(len(quenching_times2[i][j]))
+        # red = np.asarray(redshifts2[i][j])
+
+        #ax4.scatter(np.log10(1+red), ste_mass2[i][j], c=scatter_colours[i][j], label =scatter_labels[i][j], marker=scatter_markers[j])
+        #ax5.scatter(np.log10(quenching_times2[i][j]), sfr_2[i][j], edgecolor=scatter_colours[i][j], label =scatter_labels[i][j], marker=scatter_markers[j], facecolors=scatter_faces[i][j])
+# ax1.plot((0.0, 4.0),(-1.5, -1.5), 'k--')
+# ax2.plot((9.5, 11.5),(-1.5, -1.5), 'k--')
+# ax2.plot((10.0, 10.0), (-3.2, 0.0), 'k--')
+
+#x,y,ysig = myrunningmedian(red_cent, rates, 20)
+#ax3.plot(x,y, 'k--')
+
+ax1.set_xlabel(r'$z$', fontsize=16)
+ax1.set_ylabel(r'$\log(t_{q})$', fontsize=16)
+ax1.legend(loc='best', prop={'size': 10})
+ax2.set_xlabel(r'$log(M_{*})$', fontsize=16)
+ax2.set_ylabel(r'$\log(t_{q})$', fontsize=16)
+fig.tight_layout()
+fig.savefig('quenching_morethan9.5_20.png', format='png', dpi=250)
+axre.set_xlabel(r'$log(M_{*})$', fontsize=16)
+axre.set_ylabel(r'$\log(t_{q}/t_{U})$', fontsize=16)
+axre.legend(loc='best', prop={'size': 10})
+figre.tight_layout()
+figre.savefig('quenching_morethan9.5_forreport.eps', format='eps', dpi=250)
+# ax4.set_ylabel(r'$log(M_{*})$', fontsize=16)
+# ax4.set_xlabel(r'$\log(1+z)$', fontsize=16)
+# ax4.legend(loc='best', prop={'size': 10})
+# fig4.tight_layout()
+# fig4.savefig('quenching_morethan9.5_massred.png', format='png', dpi=250)
+# ax5.set_ylabel(r'$log$(SFR (M$\odot$yr$^{-1}$))', fontsize=16)
+# ax5.set_xlabel(r'$\log(t_{q})$', fontsize=16)
+# ax5.legend(loc='best', prop={'size': 10})
+# fig5.tight_layout()
+# fig5.savefig('quenching_morethan9.5_sfrtime.png', format='png', dpi=250)
+
+# fig = plt.figure(num=None, figsize=(8, 5), dpi=80, facecolor='w', edgecolor='k')
+# ax3 = fig.add_subplot(1,1,1)
+# ax3.set_xlabel(r'z', fontsize=16)
+# ax3.set_ylabel(r'$\Gamma_{Rej}$ (Gyr$^{-1}$)', fontsize=16)
+# leny = len(rates)-7
+# ax3.errorbar(red_cent[0:leny], rates[0:leny], yerr=rates_sig[0:leny], capsize=2, marker='d', markerfacecolor='None', color='k', linestyle='--', markersize=12)
+# fig.tight_layout()
+# fig.savefig('rejuvenation_rate.png', format='png', dpi=250)
+
+# fig2 = plt.figure(num=None, figsize=(10, 6), dpi=250, facecolor='w', edgecolor='k')
+# fig3 = plt.figure(num=None, figsize=(10, 6), dpi=250, facecolor='w', edgecolor='k')
+# quenchingPerType = {}
+# for i in range(0, 3):
+#     quenchingPerType['times'+str(i)] = []
+#     quenchingPerType['redshifts'+str(i)] = []
+#     quenchingPerType['mass'+str(i)] = []
+#     quenchingPerType['f_gas'+str(i)] = []
+#
+# for j in range(0, len(quenching_times2_all)):
+#     m = ste_mass2_all[j]
+#     if 9.5<=m<10.3:
+#         type = 0
+#     elif 10.3<=m<11.0:
+#         type = 1
+#     elif m>=11.0:
+#         type = 2
+#     quenchingPerType['times'+str(type)].append(quenching_times2_all[j])
+#     quenchingPerType['redshifts'+str(type)].append(redshifts2_all[j])
+#     quenchingPerType['mass'+str(type)].append(m)
+#     quenchingPerType['f_gas'+str(type)].append(frac_gas2_all[j])
+# ax1 = fig2.add_subplot(1,1,1)
+# ax2 = fig3.add_subplot(1,1,1)
+# edgcolors = ['b', 'r', 'g']
+# mass_ranges = [9.5,10.3,11.0,18.0]
+# types = [r'$9.5\leq \log(M_*) < 10.3$', r'$10.3\leq \log(M_*) < 11.0$', r'$\log(M_*) \geq 11.0$']
+# for i in range(0, len(mass_ranges)-1):
+#     red_cent,frequency,frequency_sig,times,times_sig = quenching_histogram(d['z_gal0'],galaxies,max_ngal,mass_ranges[i],mass_ranges[i+1],quenchingPerType['times'+str(i)],
+#                                                                             quenchingPerType['redshifts'+str(i)], 5)
+#     ax1.errorbar(red_cent, frequency,yerr=frequency_sig, label=types[i], marker='o', linestyle='--', capsize=3, markersize=8)
+#     ax2.errorbar(red_cent, times, yerr = times_sig, label=types[i], marker='o', linestyle='--', capsize=3, markersize=8)
+# ax1.set_ylabel('Number of quenching events per galaxy', fontsize=16)
+# ax1.set_xlabel('z', fontsize=16)
+# ax2.set_ylabel(r' $\langle \log(t_{q}/t_{U}) \rangle$', fontsize=16)
+# ax2.set_xlabel('z', fontsize=16)
+# ax2.legend(loc='best', prop={'size': 12})
+# ax1.legend(loc='best', prop={'size': 12})
+# fig2.tight_layout()
+# fig3.tight_layout()
+# fig2.savefig('quenching_histogram_2.png', format='png', dpi=200)
+# fig3.savefig('quenching_timeshisto.png', format='png', dpi=200)
+#
+# print('Quenching and Rejuvenation analysis done.')
---
+++ merger_analysis_8.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 24 December 2018
+
+@author: currorodriguez
+"""
+
+# Import required libraries
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy import stats
+from scipy.optimize import curve_fit
+import seaborn as sns
+sns.set(style="ticks")
+import sys
+sys.path.insert(0,'../Progen')
+sys.path.insert(0,'../Quench_analysis')
+from import_progen import importApp
+from quenchingFinder8_5_3 import GalaxyData
+from merger_finder_8 import merger_finder, runningmedian, runningmedian2, myrunningmedian
+
+d, ngal = importApp()
+galaxies = []
+for i in range(ngal):
+    sfr_gal = d['sfr_gal' + str(i)][::-1]
+    sfe_gal = d['sfe_gal' + str(i)][::-1]
+    z_gal = d['z_gal' + str(i)][::-1]
+    galaxy_t = d['galaxy_t' + str(i)][::-1]
+    galaxy_m = d['m_gal'+str(i)][::-1]
+    fgas_gal = d['h2_gal'+str(i)][::-1]
+    gal_type = d['gal_type'+str(i)][::-1]
+    galaxy = GalaxyData(i, sfr_gal, sfe_gal, z_gal, galaxy_t, galaxy_m, fgas_gal, gal_type)
+    galaxies.append(galaxy)
+mergers, sf_galaxies = merger_finder(galaxies, 0.2, 10**9.5, 2.5)
+
+def SF_Budget(mergers, msq_galaxies, n_bins):
+    z_bins = np.linspace(0.0, 2.5, n_bins)
+    f_budget = np.zeros(n_bins-1)
+    delta = z_bins[1]-z_bins[0]
+    z_cent = z_bins - delta/2
+    z_cent = np.delete(z_cent, 0)
+    for i in range(0, n_bins-1):
+        sfr_m = 0
+        sfr_nm = 0
+        for j in range(0, len(mergers)):
+            merger = mergers[j]
+            if z_bins[i]<= merger.z_gal < z_bins[i+1]:
+                sfr_m = sfr_m + merger.sfr_gal
+        for k in range(0, len(msq_galaxies)):
+            msq = msq_galaxies[k]
+            if z_bins[i]<= msq.z_gal < z_bins[i+1]:
+                sfr_nm = sfr_nm + msq.ssfr_gal
+        if sfr_m != 0 and sfr_nm != 0:
+            f_budget[i] = sfr_m/(sfr_m+sfr_nm)
+    plt.plot(z_cent, f_budget, linestyle='--', marker='o')
+    plt.xlabel(r'$z$')
+    plt.ylabel('Fraction of SF Budget')
+    plt.savefig('sfbudget.png', dpi=250)
+
+def SFR_Evolution(mergers, msq_galaxies, n_bins):
+    z_bins = np.linspace(0.0, 2.5, n_bins)
+    sfr_m_ave = np.zeros(n_bins-1)
+    sfr_m_error = np.zeros(n_bins-1)
+    sfr_nm_ave = np.zeros(n_bins-1)
+    sfr_nm_error = np.zeros(n_bins-1)
+    delta = z_bins[1]-z_bins[0]
+    z_cent = z_bins - delta/2
+    z_cent = np.delete(z_cent, 0)
+    for i in range(0, n_bins-1):
+        sfr_m = []
+        sfr_nm = []
+        for j in range(0, len(mergers)):
+            merger = mergers[j]
+            if z_bins[i]<= merger.z_gal < z_bins[i+1]:
+                sfr_m.append(np.log10(merger.sfr_gal/merger.m_gal))
+        for k in range(0, len(msq_galaxies)):
+            msq = msq_galaxies[k]
+            if z_bins[i]<= msq.z_gal < z_bins[i+1]:
+                sfr_nm.append(np.log10(msq.ssfr_gal/msq.m_gal))
+        sfr_m = np.asarray(sfr_m)
+        sfr_nm = np.asarray(sfr_nm)
+        sfr_m_ave[i] = np.average(sfr_m)
+        sfr_m_error[i] = np.std(sfr_m)/np.sqrt(np.log10(len(sfr_m)))
+        sfr_nm_ave[i] = np.average(sfr_nm)
+        sfr_nm_error[i] = np.std(sfr_nm)/np.sqrt(np.log10(len(sfr_nm)))
+    plt.errorbar(z_cent, sfr_m_ave, yerr=sfr_m_error, linestyle='--', marker='o', label='Mergers star-forming', capsize=2, capthick=2)
+    plt.errorbar(z_cent, sfr_nm_ave, yerr=sfr_nm_error, linestyle='--', marker='s', label='Non-mergers star-forming ', capsize=2, capthick=2)
+    plt.xlabel(r'$z$')
+    plt.ylabel(r'$\log(\langle$sSFR (yr$^{-1}\rangle)$')
+    plt.tight_layout()
+    plt.legend(loc='best')
+    plt.savefig('sfr_evolution.png', dpi=250)
+def SFR_Evolution2(mergers, msq_galaxies, n_bins):
+    z_bins = np.linspace(0.0, 2.5, n_bins)
+    ssfr_m_ave = np.zeros(n_bins-1)
+    ssfr_m_error = np.zeros(n_bins-1)
+    ssfr_nm_ave = np.zeros(n_bins-1)
+    ssfr_nm_error = np.zeros(n_bins-1)
+    delta = z_bins[1]-z_bins[0]
+    z_cent = z_bins - delta/2
+    z_cent = np.delete(z_cent, 0)
+    for i in range(0, n_bins-1):
+        ssfr_m = []
+        ssfr_nm = []
+        mergers_m = []
+        msq_m = []
+        msq_idx = []
+        for j in range(0, len(mergers)):
+            merger = mergers[j]
+            if z_bins[i]<= merger.z_gal < z_bins[i+1]:
+                ssfr_m.append(np.log10(merger.sfr_gal/merger.m_gal))
+                mergers_m.append(np.log10(merger.m_gal))
+        for k in range(0, len(msq_galaxies)):
+            msq = msq_galaxies[k]
+            if z_bins[i]<= msq.z_gal < z_bins[i+1]:
+                msq_idx.append(k)
+                msq_m.append(np.log10(msq.m_gal))
+        ssfr_m = np.asarray(ssfr_m)
+        mergers_m = np.asarray(mergers_m)
+        msq_idx = np.asarray(msq_idx)
+        msq_m = np.asarray(msq_m)
+        msq_sort_idx = np.argsort(msq_m)
+        msq_sorted = np.sort(msq_m)
+        msq_sel = []
+        for m in range(0, len(mergers_m)):
+            if len(msq_sorted)>3:
+                loc = np.searchsorted(msq_sorted, mergers_m[m])
+                below = 30
+                above = 30
+                if loc - below < 0:
+                    below = loc
+                if loc + above > len(msq_sorted):
+                    above  = len(msq_sorted) - loc
+                msq_slice = msq_sorted[loc-below:loc+above]
+                msq_diff = msq_slice - mergers_m[m]
+                for difmin in range(0, 8):
+                    min_idx = np.argmin(msq_diff)
+                    idx = loc - below + min_idx
+                    msq_sel.append(msq_sort_idx[idx])
+                    msq_diff = np.delete(msq_diff, min_idx)
+                    msq_sorted = np.delete(msq_sorted, idx)
+                    msq_sort_idx = np.delete(msq_sort_idx, idx)
+        for selected  in range(0, len(msq_sel)):
+            real_idx = msq_idx[msq_sel[selected]]
+            msq_gal = msq_galaxies[real_idx]
+            ssfr_nm.append(np.log10(msq_gal.ssfr_gal/msq_gal.m_gal))
+        ssfr_nm = np.asarray(ssfr_nm)
+        ssfr_m_ave[i] = np.average(ssfr_m)
+        ssfr_m_error[i] = np.std(ssfr_m)/np.sqrt(np.log10(len(ssfr_m)))
+        ssfr_nm_ave[i] = np.average(ssfr_nm)
+        ssfr_nm_error[i] = np.std(ssfr_nm)/np.sqrt(np.log10(len(ssfr_nm)))
+    plt.errorbar(z_cent, ssfr_m_ave, yerr=ssfr_m_error, linestyle='--', marker='o', label='Mergers star-forming', capsize=2, capthick=2)
+    plt.errorbar(z_cent, ssfr_nm_ave, yerr=ssfr_nm_error, linestyle='--', marker='s', label='Mass-matched sample of non-merger star-forming', capsize=2, capthick=2)
+    plt.xlabel(r'$z$')
+    plt.ylabel(r'$\log(\langle$sSFR (yr$^{-1}\rangle)$')
+    plt.tight_layout()
+    plt.legend(loc='best')
+    plt.savefig('sfr_evolution_2.eps', dpi=250)
+def Merger_Fraction(mergers, msq_galaxies, n_bins):
+    z_bins = np.linspace(0.0, 2.5, n_bins)
+    f_merger = np.zeros(n_bins-1)
+    delta = z_bins[1]-z_bins[0]
+    z_cent = z_bins - delta/2
+    z_cent = np.delete(z_cent, 0)
+    for i in range(0, n_bins-1):
+        m_counter = 0
+        nm_counter = 0
+        for j in range(0, len(mergers)):
+            merger = mergers[j]
+            if z_bins[i]<= merger.z_gal < z_bins[i+1]:
+                m_counter = m_counter + 1
+        for k in range(0, len(msq_galaxies)):
+            msq = msq_galaxies[k]
+            if z_bins[i]<= msq.z_gal < z_bins[i+1]:
+                nm_counter = nm_counter + 1
+        f_merger[i] = m_counter/(m_counter+nm_counter)
+    plt.plot(z_cent, f_merger, linestyle='--', marker='o')
+    plt.xlabel(r'$z$')
+    plt.ylabel('Merger fraction of star-forming galaxies')
+    plt.savefig('mfr_evolution.png', dpi=250)
+def Merger_Contribution(mergers, msq_galaxies, n_bins):
+    z_bins = np.linspace(0.0, 2.5, n_bins)
+    f_merger = np.zeros(n_bins-1)
+    f_budget = np.zeros(n_bins-1)
+    delta = z_bins[1]-z_bins[0]
+    z_cent = z_bins - delta/2
+    z_cent = np.delete(z_cent, 0)
+    for i in range(0, n_bins-1):
+        m_counter = 0
+        nm_counter = 0
+        sfr_m = 0
+        sfr_nm = 0
+        for j in range(0, len(mergers)):
+            merger = mergers[j]
+            if z_bins[i]<= merger.z_gal < z_bins[i+1]:
+                sfr_m = sfr_m + merger.sfr_gal
+                m_counter = m_counter + 1
+        for k in range(0, len(msq_galaxies)):
+            msq = msq_galaxies[k]
+            if z_bins[i]<= msq.z_gal < z_bins[i+1]:
+                sfr_nm = sfr_nm + msq.ssfr_gal
+                nm_counter = nm_counter + 1
+        f_merger[i] = m_counter/(m_counter+nm_counter)
+        f_budget[i] = sfr_m/(sfr_m+sfr_nm)
+    fig = plt.figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
+    ax = fig.add_subplot(1,1,1)
+    ax.set_xscale("log")
+    ax.set_xticks([0.0, 0.5, 1.0, 2.5])
+    ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    ax.set_yscale("log")
+    plt.plot(z_cent, f_merger, linestyle='--', marker='o', label='Fraction of galaxies')
+    plt.plot(z_cent, f_budget, linestyle='--', marker='s', label='Fraction of SF Budget')
+    ax.set_xlabel(r'$z$', fontsize=16)
+    ax.set_ylabel('Merger contribution to star-forming galaxies', fontsize=16)
+    ax.legend(loc='best')
+    fig.tight_layout()
+    fig.savefig('merger_contribution.png',format='png', dpi=250)
+
+def Fgas_mean(mergers, msq_galaxies, n_bins):
+    z_bins = np.linspace(0.0, 2.5, n_bins)
+    fgas_ave = np.zeros(n_bins-1)
+    m_ave = np.zeros(n_bins-1)
+    fgas_error = np.zeros(n_bins-1)
+    delta = z_bins[1]-z_bins[0]
+    z_cent = z_bins - delta/2
+    z_cent = np.delete(z_cent, 0)
+    for i in range(0, n_bins-1):
+        fgas = []
+        mass = []
+        for j in range(0, len(mergers)):
+            merger = mergers[j]
+            if z_bins[i]<= merger.z_gal < z_bins[i+1]:
+                fgas.append(merger.fgas_gal)
+                mass.append(merger.m_gal)
+        mass = np.asarray(mass)
+        m_ave[i] = np.average(mass)
+        fgas = np.asarray(fgas)
+        fgas_ave[i] = np.average(fgas)
+        fgas_error[i] = np.std(fgas)/np.sqrt(len(fgas))
+    model_1 = 0.04*(m_ave/(4.5e+11))**(-0.59*(1+z_cent)**(0.45))
+    fig = plt.figure(num=None, figsize=(8, 4), dpi=80, facecolor='w', edgecolor='k')
+    ax = fig.add_subplot(1,1,1)
+    ax.set_xscale("log")
+    ax.set_xticks([0.0, 0.5, 1.0, 2.5])
+    ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    ax.set_yscale("log")
+    plt.errorbar(z_cent, fgas_ave, yerr=fgas_error, linestyle='--', marker='o', capsize=2, capthick=2, label=r'Mergers in m50n512')
+    plt.plot(z_cent, model_1,  'k-', label='Stewart et al. 2009b')
+    plt.errorbar([1.465, 1.523, 1.522, 1.414, 1.6, 1.459],
+                    [0.66, 0.51, 0.58, 0.62, 0.52,0.56],
+                    yerr=[0.09*0.66, 0.09*0.51, 0.09*0.58, 0.09*0.62, 0.09*0.52, 0.09*0.56],
+                    label='Daddi et al. 2010', marker='d', capsize=2, linestyle=' ',markerfacecolor='None')
+    plt.errorbar([1.2,1.17,1.18,1.12,1.19,1.23,1.28,1.12,1.1,2.19,2.33,2.43,2.29,2.34,2.34,2.19,2.17,2.18,2.21],
+                    [0.46,0.34,0.47,0.27,0.45,0.46,0.18,0.14,0.26,0.51,0.73,0.42,0.36,0.78,0.4,0.32,0.33,0.41,0.57],
+                    yerr=[0.19,0.14,0.19,0.11,0.2,0.19,0.08,0.07,0.12,0.26,0.32,0.22,0.15,0.33,0.17,0.16,0.17,0.18,0.23],
+                    label='Tacconi et al. 2010', marker='s', capsize=2, linestyle=' ',markerfacecolor='None')
+    ax.set_xlabel(r'$z$', fontsize=16)
+    ax.set_ylabel(r'$\langle f_{H_2}\rangle$', fontsize=16)
+    ax.legend(loc='best')
+    fig.tight_layout()
+    fig.savefig('fgas_evolution.png',format='png', dpi=250)
+
+def Frac_Merger_rate(mergers, msq_galaxies, n_bins):
+    z_bins = np.linspace(0.0, 2.5, n_bins)
+    f_merger = np.zeros(n_bins-1)
+    delta = z_bins[1]-z_bins[0]
+    z_cent = z_bins - delta/2
+    z_cent = np.delete(z_cent, 0)
+    for i in range(0, n_bins-1):
+        m_counter = 0
+        nm_counter = 0
+        times = []
+        for j in range(0, len(mergers)):
+            merger = mergers[j]
+            if z_bins[i]<= merger.z_gal < z_bins[i+1]:
+                m_counter = m_counter + 1
+                times.append(merger.galaxy_t)
+        for k in range(0, len(msq_galaxies)):
+            msq = msq_galaxies[k]
+            if z_bins[i]<= msq.z_gal < z_bins[i+1]:
+                nm_counter = nm_counter + 1
+        times = np.asarray(times)
+        delta_t = times.max() - times.min()
+        f_merger[i] = m_counter/((m_counter+nm_counter)*delta_t)
+    z_cent = np.asarray(z_cent)
+    f_merger = np.asarray(f_merger)
+    x = np.log10(1+z_cent)
+    y = np.log10(f_merger)
+    def func(x, a, b):
+        return a*x + b
+    # popt, pcov = curve_fit(func, x, y)
+    # perr = np.sqrt(np.diag(pcov))
+    # print(popt)
+    # print(perr)
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+    print("slope: %f    intercept: %f    r_value: %f    p_value: %f    std_error: %f" % (slope, intercept,r_value, p_value, std_err))
+    fig = plt.figure(num=None, figsize=(8, 6), dpi=80, facecolor='w', edgecolor='k')
+    ax = fig.add_subplot(1,1,1)
+    #ax.set_xscale("log")
+    #ax.set_xticks([0.0, 0.5, 1.0, 2.5])
+    #ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    #ax.set_yscale("log")
+    plt.plot(1+z_cent, f_merger, linestyle='--', marker='d', label=r'Mergers in the $50h^{-1}$ Mpc box')
+    plt.plot(1+z_cent,(10**intercept)*(1+z_cent)**(slope), 'k-', label='The best fit power law to this data')
+    ax.set_xlabel(r'$1+z$', fontsize=16)
+    ax.set_ylabel(r'$\mathcal{R}_{merg}$ [Gyr$^{-1}$]', fontsize=16)
+    ax.legend(loc='best', prop={'size': 12})
+    fig.tight_layout()
+    fig.savefig('merger_rate_evolution.png',format='png', dpi=250)
+#SF_Budget(mergers, sf_galaxies, 10)
+#SFR_Evolution(mergers, sf_galaxies, 10)
+#Merger_Fraction(mergers, sf_galaxies, 10)
+#Merger_Contribution(mergers, sf_galaxies, 10)
+#Fgas_mean(mergers, sf_galaxies, 10)
+#Frac_Merger_rate(mergers, sf_galaxies, 15)
+SFR_Evolution2(mergers, sf_galaxies, 10)
