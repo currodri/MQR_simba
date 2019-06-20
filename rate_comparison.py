de4934671b3354d0bdef65026a3488aa68db1eea
Curro Rodriguez <s1650043@ed.ac.uk>
2019-06-20 18:25:43 +0200

    Correction 5

--- rate_comparison.py
+++ rate_comparison.py
@@ -162,28 +162,28 @@ def Fractional_Rate(mergers,sf_galaxies,q_masses,q_reds,q_thubble,reju_z,reju_t,
             merger = mergers[j]
             if z_bins[i]<= merger.z_gal[1] < z_bins[i+1]:
                 type = Mass_Bin_Type(mass_limits,merger.m_gal[1])
-                if type =! False:
+                if type != False:
                     r_merger['massbin'+str(type)][i] = r_merger['massbin'+str(type)][i] + 1
                     times.append(merger.galaxy_t[1])
         for k in range(0, len(sf_galaxies)):
             sf = sf_galaxies[k]
             if z_bins[i]<= sf.z_gal < z_bins[i+1]:
                 type = Mass_Bin_Type(mass_limits,sf.m_gal)
-                if type =! False:
+                if type != False:
                     sf_counter = sf_counter + 1
                     times.append(sf.galaxy_t)
         for l in range(0, len(q_reds)):
             quench_red = q_reds[l]
             if z_bins[i]<= quench_red < z_bins[i+1]:
                 type = Mass_Bin_Type(mass_limits,q_masses[l])
-                if type =! False:
+                if type != False:
                     r_quench['massbin'+str(type)][i] = r_quench['massbin'+str(type)][i] + 1
                     times.append(q_thubble[l])
         for m in range(0, len(reju_z)):
             reju = reju_z[m]
             if z_bins[i]<= reju < z_bins[i+1]:
                 type = Mass_Bin_Type(mass_limits, reju_m[m])
-                if type =! False:
+                if type != False:
                     r_reju['massbin'+str(type)][i] = r_reju['massbin'+str(type)][i] + 1
                     times.append(reju_t[m])
         times = np.asarray(times)
