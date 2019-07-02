ee15bddf939ea12a9ab86516710e5840ac82c598
Curro Rodriguez <s1650043@ed.ac.uk>
2019-07-02 13:07:33 +0100

    Corrections 7

--- quench_rejuvenation.py
+++ quench_rejuvenation.py
@@ -165,7 +165,6 @@ def Fraction_Fast_vs_Slow(x, times, sf_d, bins):
         for j in range(0, len(fast)):
             if bins[i] <= fast[j] < bins[i+1]:
                 f = f + 1
-        print(i, f, s, sf)
         fast_bins[i] = float(f)/float(sf)
         slow_bins[i] = float(s)/float(sf)
     return fast_bins,slow_bins,cent
@@ -179,13 +178,12 @@ def Quenching_Scatter_Plot(redshifts, quenching_times, ste_mass):
     sf_x = [d['redshifts'],np.log10(d['sf_galaxies_mass'])]
     x_data = [redshifts, ste_mass]
     for i in range(0, len(x_labels)):
-        print(i)
         fig, ax = plt.subplots(3, 1, sharex='col', num=None, figsize=(8, 9), dpi=80, facecolor='w', edgecolor='k')
         for j in range(0, len(y_labels)):
             ax[j].set_ylabel(y_labels[j], fontsize=16)
             if j!=2:
-                ax[j].hexbin(x_data[i][j][0], quenching_times[j][0], bins='log', cmap='Greys')
-                ax[j].scatter(x_data[i][j][2], quenching_times[j][2], s=8, alpha=0.8, label='Final quenching with rejuvenation', facecolor='b')
+                ax[j].hexbin(x_data[i][j][0], quenching_times[j][0], bins='log', cmap='Greys', gridsize=30)
+                ax[j].scatter(x_data[i][j][2], quenching_times[j][2], s=8, alpha=0.8, label='Final quenching with rejuvenation', facecolor=None, edgecolor='b')
                 ax[j].legend(loc='best', prop={'size': 10})
             else:
                 for k in range(0, 2):
@@ -200,7 +198,6 @@ def Quenching_Scatter_Plot(redshifts, quenching_times, ste_mass):
                         sf_data = [sf_x[i]]
                         bins = np.linspace(9.5,12.5,10)
                     fast, slow, cent = Fraction_Fast_vs_Slow(x_datas, quenchs, sf_data, bins)
-                    print(fast, slow, cent)
                     ax[j].plot(cent, np.log10(fast), label = frac_labels[k]+'fast quenching')
                     ax[j].plot(cent, np.log10(slow), label = frac_labels[k]+'slow quenching')
                 ax[j].legend(loc='best', prop={'size': 10})
