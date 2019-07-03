cd3fda721daa593eedb3640849b74c3fe8f1e30d
Curro Rodriguez <s1650043@ed.ac.uk>
2019-07-03 22:51:07 +0100

    New ticks

--- quench_rejuvenation.py
+++ quench_rejuvenation.py
@@ -46,8 +46,7 @@ for i in range(ngal):
     galaxy_m = d['m_gal'+str(i)][::-1]
     fgas_gal = d['h2_gal'+str(i)][::-1]
     gal_type = d['gal_type'+str(i)][::-1]
-    #gal_pos = d['gal_pos'+str(i)][::-1]
-    gal_pos = None
+    gal_pos = d['gal_pos'+str(i)][::-1]
     galaxy = GalaxyData(i, sfr_gal, sfe_gal, z_gal, galaxy_t, galaxy_m, fgas_gal, gal_type, gal_pos)
     galaxies.append(galaxy)
 
