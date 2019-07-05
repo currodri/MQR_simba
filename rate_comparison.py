4587c702e779d1b96f9cc154b83bf83288ee7b06
Curro Rodriguez <s1650043@ed.ac.uk>
2019-07-05 12:35:54 +0100

    New rate lines Fitting for total rates

--- rate_comparison.py
+++ rate_comparison.py
@@ -155,6 +155,9 @@ def Fractional_Rate(mergers,sf_galaxies,q_masses,q_reds,q_thubble,reju_z,reju_t,
     r_merger = {}
     r_quench = {}
     r_reju = {}
+    r_merger['all'] = np.zeros(n_bins-1)
+    r_quench['all'] = np.zeros(n_bins-1)
+    r_reju['all'] = np.zeros(n_bins-1)
     for bini in range(0, len(mass_limits)):
         r_merger['massbin'+str(bini)] = np.zeros(n_bins-1)
         r_quench['massbin'+str(bini)] = np.zeros(n_bins-1)
@@ -200,6 +203,8 @@ def Fractional_Rate(mergers,sf_galaxies,q_masses,q_reds,q_thubble,reju_z,reju_t,
                     times.append(reju_t[m])
         times = np.asarray(times)
         delta_t = float(times.max() - times.min())
+        a = 0
+        b = 0
         for ty in range(0, len(mass_limits)):
             print(counter)
             print(r_merger['massbin'+str(ty)][i], r_quench['massbin'+str(ty)][i], r_merger['massbin'+str(ty)][i])
@@ -207,25 +212,50 @@ def Fractional_Rate(mergers,sf_galaxies,q_masses,q_reds,q_thubble,reju_z,reju_t,
             r_merger['massbin'+str(ty)][i] = float(r_merger['massbin'+str(ty)][i])/normalization
             r_quench['massbin'+str(ty)][i] = float(r_quench['massbin'+str(ty)][i])/normalization
             r_reju['massbin'+str(ty)][i] = float(r_reju['massbin'+str(ty)][i])/normalization
+            a = a + r_merger['massbin'+str(ty)][i]
+            b = b + r_quench['massbin'+str(ty)][i]
+            c = c + r_reju['massbin'+str(ty)][i]
+            sf = sf + sf_counter['massbin'+str(ty)]
+        normalization = float(float(a+b)*delta_t)
+        r_merger['all'][i] = float(a)/normalization
+        r_quench['all'][i] = float(b)/normalization
+        r_reju['all'][i] = float(c)/normalization
     fig, ax = plt.subplots(3, 1, sharex='col', num=None, figsize=(8, 10), dpi=80, facecolor='w', edgecolor='k')
     x_dat = np.log10(1+z_cent)
     for i in range(0, len(mass_limits)):
         ax[0].plot(x_dat, np.log10(r_merger['massbin'+str(i)]), linestyle='--', marker='d', label=mass_labels[i])
         ax[1].plot(x_dat, np.log10(r_quench['massbin'+str(i)]), linestyle='--', marker='d')
         ax[2].plot(x_dat, np.log10(r_reju['massbin'+str(i)]), linestyle='--', marker='d')
+
+    ax[0].plot(x_dat, np.log10(r_merger['all']),linestyle='--', marker='d', color='k', label='All')
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x_dat, np.log10(r_merger['all']))
+    ax[0].plot(x_dat,np.log10((10**intercept)*(1+z_cent)**(slope)), 'k-', label=r'$10^{%f}*(1+z)^{%f}$' % (intercept, slope) )
+    print("slope: %f    intercept: %f    r_value: %f    p_value: %f    std_error: %f" % (slope, intercept,r_value, p_value, std_err))
+
+    ax[1].plot(x_dat, np.log10(r_quench['all']),linestyle='--', marker='d', color='k')
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x_dat, np.log10(r_quench['all']))
+    ax[1].plot(x_dat,np.log10((10**intercept)*(1+z_cent)**(slope)), 'k-', label=r'$10^{%f}*(1+z)^{%f}$' % (intercept, slope) )
+    print("slope: %f    intercept: %f    r_value: %f    p_value: %f    std_error: %f" % (slope, intercept,r_value, p_value, std_err))
+
+    ax[2].plot(x_dat, np.log10(r_reju['all']),linestyle='--', marker='d', color='k')
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x_dat, np.log10(r_reju['all']))
+    ax[1].plot(x_dat,np.log10((10**intercept)*(1+z_cent)**(slope)), 'k-', label=r'$10^{%f}*(1+z)^{%f}$' % (intercept, slope) )
+    print("slope: %f    intercept: %f    r_value: %f    p_value: %f    std_error: %f" % (slope, intercept,r_value, p_value, std_err))
     ax[0].set_ylabel(r'$\log(\mathcal{R}_{Mer})$ [Gyr$^{-1}$]', fontsize=16)
     ax[1].set_ylabel(r'$\log(\mathcal{R}_{Que})$ [Gyr$^{-1}$]', fontsize=16)
     ax[2].set_ylabel(r'$\log(\mathcal{R}_{Rej})$ [Gyr$^{-1}$]', fontsize=16)
     ax[2].set_xlabel(r'$\log(1+z)$', fontsize=16)
     axR = ax[0].twiny()
-    topticks1 = np.array([0,1,2,4,6])  # desired redshift labels
+    topticks1 = np.array([0,1,2,3,4])  # desired redshift labels
     topticks2 = np.log10(1+topticks1)  # tick locations in time
     axR.set_xticklabels(topticks1)
     axR.set_xticks(topticks2)
     axR.xaxis.set_ticks_position('top') # set the position of the second x-axis to top
     axR.xaxis.set_label_position('top') # set the position of the second x-axis to top
     axR.set_xlabel('z', fontsize=16)
-    ax[0].legend(loc='best', prop={'size': 12})
+    ax[0].legend(loc='best', prop={'size': 8})
+    ax[1].legend(loc='best', prop={'size': 8})
+    ax[2].legend(loc='best', prop={'size': 8})
     fig.subplots_adjust(hspace=0)
     fig.savefig(str(results_folder)+'mqr_fractional_rate.png', format='png', dpi=200)
 
@@ -236,6 +266,9 @@ def Density_Rate(mergers,q_masses,q_reds,q_thubble,reju_z,reju_t,reju_m,n_bins,m
     r_merger = {}
     r_quench = {}
     r_reju = {}
+    r_merger['all'] = np.zeros(n_bins-1)
+    r_quench['all'] = np.zeros(n_bins-1)
+    r_reju['all'] = np.zeros(n_bins-1)
     for bini in range(0, len(mass_limits)):
         r_merger['massbin'+str(bini)] = np.zeros(n_bins-1)
         r_quench['massbin'+str(bini)] = np.zeros(n_bins-1)
@@ -267,10 +300,23 @@ def Density_Rate(mergers,q_masses,q_reds,q_thubble,reju_z,reju_t,reju_m,n_bins,m
                     r_reju['massbin'+str(type)][i] = r_reju['massbin'+str(type)][i] + 1
                     counter.append(reju)
         total_v = float(100*len(np.unique(counter)))
+        a = 0
+        b = 0
         for ty in range(0, len(mass_limits)):
-            r_merger['massbin'+str(ty)][i] = float(r_merger['massbin'+str(ty)][i])/total_v
-            r_quench['massbin'+str(ty)][i] = float(r_quench['massbin'+str(ty)][i])/total_v
-            r_reju['massbin'+str(ty)][i] = float(r_reju['massbin'+str(ty)][i])/total_v
+            print(counter)
+            print(r_merger['massbin'+str(ty)][i], r_quench['massbin'+str(ty)][i], r_merger['massbin'+str(ty)][i])
+            normalization = float(float(r_merger['massbin'+str(ty)][i]+sf_counter['massbin'+str(ty)])*delta_t)
+            r_merger['massbin'+str(ty)][i] = float(r_merger['massbin'+str(ty)][i])/normalization
+            r_quench['massbin'+str(ty)][i] = float(r_quench['massbin'+str(ty)][i])/normalization
+            r_reju['massbin'+str(ty)][i] = float(r_reju['massbin'+str(ty)][i])/normalization
+            a = a + r_merger['massbin'+str(ty)][i]
+            b = b + r_quench['massbin'+str(ty)][i]
+            c = c + r_reju['massbin'+str(ty)][i]
+            sf = sf + sf_counter['massbin'+str(ty)]
+        normalization = float(float(a+b)*delta_t)
+        r_merger['all'][i] = float(a)/normalization
+        r_quench['all'][i] = float(b)/normalization
+        r_reju['all'][i] = float(c)/normalization
     fig, ax = plt.subplots(3, 1, sharex='col', num=None, figsize=(8, 10), dpi=80, facecolor='w', edgecolor='k')
     x_dat = np.log10(1+z_cent)
     for i in range(0, len(mass_limits)):
@@ -281,15 +327,31 @@ def Density_Rate(mergers,q_masses,q_reds,q_thubble,reju_z,reju_t,reju_m,n_bins,m
     ax[1].set_ylabel(r'$\log(\Gamma_{Que})$ [cMpc$^{-3}$Gyr$^{-1}$]', fontsize=16)
     ax[2].set_ylabel(r'$\log(\Gamma_{Rej})$ [cMpc$^{-3}$Gyr$^{-1}$]', fontsize=16)
     ax[2].set_xlabel(r'$\log(1+z)$', fontsize=16)
+    ax[0].plot(x_dat, np.log10(r_merger['all']),linestyle='--', marker='d', color='k', label='All')
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x_dat, np.log10(r_merger['all']))
+    ax[0].plot(x_dat,np.log10((10**intercept)*(1+z_cent)**(slope)), 'k-', label=r'$10^{%f}*(1+z)^{%f}$' % (intercept, slope) )
+    print("slope: %f    intercept: %f    r_value: %f    p_value: %f    std_error: %f" % (slope, intercept,r_value, p_value, std_err))
+
+    ax[1].plot(x_dat, np.log10(r_quench['all']),linestyle='--', marker='d', color='k')
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x_dat, np.log10(r_quench['all']))
+    ax[1].plot(x_dat,np.log10((10**intercept)*(1+z_cent)**(slope)), 'k-', label=r'$10^{%f}*(1+z)^{%f}$' % (intercept, slope) )
+    print("slope: %f    intercept: %f    r_value: %f    p_value: %f    std_error: %f" % (slope, intercept,r_value, p_value, std_err))
+
+    ax[2].plot(x_dat, np.log10(r_reju['all']),linestyle='--', marker='d', color='k')
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x_dat, np.log10(r_reju['all']))
+    ax[1].plot(x_dat,np.log10((10**intercept)*(1+z_cent)**(slope)), 'k-', label=r'$10^{%f}*(1+z)^{%f}$' % (intercept, slope) )
+    print("slope: %f    intercept: %f    r_value: %f    p_value: %f    std_error: %f" % (slope, intercept,r_value, p_value, std_err))
     axR = ax[0].twiny()
-    topticks1 = np.array([0,1,2,4,6])  # desired redshift labels
+    topticks1 = np.array([0,1,2,3,4])  # desired redshift labels
     topticks2 = np.log10(1+topticks1)  # tick locations in time
     axR.set_xticklabels(topticks1)
     axR.set_xticks(topticks2)
     axR.xaxis.set_ticks_position('top') # set the position of the second x-axis to top
     axR.xaxis.set_label_position('top') # set the position of the second x-axis to top
     axR.set_xlabel('z', fontsize=16)
-    ax[0].legend(loc='best', prop={'size': 12})
+    ax[0].legend(loc='best', prop={'size': 8})
+    ax[1].legend(loc='best', prop={'size': 8})
+    ax[2].legend(loc='best', prop={'size': 8})
     fig.subplots_adjust(hspace=0)
     fig.savefig(str(results_folder)+'mqr_density_rate.png', format='png', dpi=200)
 
