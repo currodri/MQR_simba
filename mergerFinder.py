16c34df1de87bfa00ffeb2441aa5b66f2b96de06
Curro Rodriguez <s1650043@ed.ac.uk>
2019-07-11 10:34:47 +0100

    Change merger condition

--- mergerFinder.py
+++ mergerFinder.py
@@ -82,8 +82,8 @@ def merger_finder(galaxies, merger_ratio, mass_limit, redshift_limit):
                 delta_t = time[i+1]-time[i]
                 condition,ratio = merger_condition(sfr[i], delta_t, mass, i, merger_ratio, mass_limit)
                 sfcondition = sfr_condition_2('end', gal, i)
-                ssfr = sfr[i+1]/mass[i+1]
-                if condition == True and ssfr>=(10**sfcondition) and fgas[i+1]>0:
+                ssfr = sfr[i+2]/mass[i+2]
+                if condition == True and ssfr>=(10**sfcondition) and fgas[i+2]>0:
                     boost = (fgas[i+1]-fgas[i-1])/fgas[i-1]
                     # Save data at the merger, after and before
                     merger = Merger(id,sfr[i-1:i+2],sfe[i-1:i+2],z[i-1:i+2],time[i-1:i+2],mass[i-1:i+2],fgas[i-1:i+2],type[i-1:i+2],pos[i-1:i+2],ratio, boost)
