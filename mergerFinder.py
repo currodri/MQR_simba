172e3de6283f16c737f728e0f730905f76efa370
Curro Rodriguez <s1650043@ed.ac.uk>
2019-07-11 10:37:12 +0100

    Change merger condition

--- mergerFinder.py
+++ mergerFinder.py
@@ -39,15 +39,15 @@ def merger_condition(sfr, delta_t, mass_list, index, merger_ratio, mass_limit):
     condition = False
     predicted = sfr*delta_t*(10**9)
     actual = mass_list[index+1] - mass_list[index]
-    #print(predicted, actual)
+    print(predicted, actual)
     diff = (mass_list[index+1]-mass_list[index])/mass_list[index]
     diff2 = abs((mass_list[index+2]-mass_list[index])/mass_list[index])
     diff3 = abs((mass_list[index+1]-mass_list[index-1])/mass_list[index-1])
     diff4 = abs((mass_list[index+3]-mass_list[index])/mass_list[index])
-    #print(diff, diff2, diff-diff3, diff4)
+    print(diff, diff2, diff-diff3, diff4)
     if diff>=merger_ratio and diff2>=merger_ratio and predicted <= 0.25*actual and diff-diff3 < 0.001 and diff4>=merger_ratio and mass_list[index]>=mass_limit:
         condition = True
-        #print('hey')
+        print('hey')
     return (condition, diff)
 ###########################################################################################
 """
@@ -83,7 +83,8 @@ def merger_finder(galaxies, merger_ratio, mass_limit, redshift_limit):
                 condition,ratio = merger_condition(sfr[i], delta_t, mass, i, merger_ratio, mass_limit)
                 sfcondition = sfr_condition_2('end', gal, i)
                 ssfr = sfr[i+2]/mass[i+2]
-                if condition == True and ssfr>=(10**sfcondition) and fgas[i+2]>0:
+                print(sfcondition, ssfr)
+                if condition == True and ssfr>=(10**sfcondition): #and fgas[i+2]>0:
                     boost = (fgas[i+1]-fgas[i-1])/fgas[i-1]
                     # Save data at the merger, after and before
                     merger = Merger(id,sfr[i-1:i+2],sfe[i-1:i+2],z[i-1:i+2],time[i-1:i+2],mass[i-1:i+2],fgas[i-1:i+2],type[i-1:i+2],pos[i-1:i+2],ratio, boost)
