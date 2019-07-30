e1a1bc3903566a7e50453021d6149a4a757e6f18
Curro Rodriguez <s1650043@ed.ac.uk>
2019-07-30 18:39:03 +0100

    Correction

--- quenchingFinder.py
+++ quenchingFinder.py
@@ -268,7 +268,7 @@ def ssfr_interpolation(galaxy):
                                 galaxy.z_gal[quench.below11],time_new.tolist(), galaxy.m_gal[quench.above9],
                                 galaxy.fgas_gal[quench.above9], quench.type, None, galaxy.caesar_id)
         new_gal.rate = galaxy.rate
-        new_gal.z_gal = galaxy.z_gal
+        new_gal.all_z = galaxy.z_gal
 
         new_galaxies.append(new_gal)
     return new_galaxies
