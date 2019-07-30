16673ef5b0e66b0d54849cb538e06482f322ceff
Curro Rodriguez <s1650043@ed.ac.uk>
2019-07-30 16:29:15 +0100

    Correction

--- progen_extractor.py
+++ progen_extractor.py
@@ -75,86 +75,85 @@ def sfr_condition(type, time):
 
 for s in range(0, len(progenref_data[0])+1):
 
-    if str(caesarfile+snaps_sorted[s]) == 'm50n512_116.hdf5':
-        continue
-    sim = caesar.load(caesarfile+snaps_sorted[s],LoadHalo=False) # load caesar file
+    if caesarfile+snaps_sorted[s] != 'm50n512_116.hdf5':
+        sim = caesar.load(caesarfile+snaps_sorted[s],LoadHalo=False) # load caesar file
 
-    # initialize simulation parameters
-    redshift = sim.simulation.redshift  # this is the redshift of the simulation output
-    h = sim.simulation.hubble_constant  # this is the hubble parameter = H0/100
-    cosmo = FlatLambdaCDM(H0=100*sim.simulation.hubble_constant, Om0=sim.simulation.omega_matter, Ob0=sim.simulation.omega_baryon,Tcmb0=2.73)  # set our cosmological parameters
-    thubble = cosmo.age(redshift).value  # age of universe at this redshift
+        # initialize simulation parameters
+        redshift = sim.simulation.redshift  # this is the redshift of the simulation output
+        h = sim.simulation.hubble_constant  # this is the hubble parameter = H0/100
+        cosmo = FlatLambdaCDM(H0=100*sim.simulation.hubble_constant, Om0=sim.simulation.omega_matter, Ob0=sim.simulation.omega_baryon,Tcmb0=2.73)  # set our cosmological parameters
+        thubble = cosmo.age(redshift).value  # age of universe at this redshift
 
-    # Get galaxy info
-    gals = np.asarray([i.central for i in sim.galaxies])   # read in galaxies from caesar file
-    ms = np.asarray([i.masses['stellar'] for i in sim.galaxies])   # read in stellar masses of galaxies
-    mHI = np.asarray([i.masses['HI'] for i in sim.galaxies])   # read in neutral hydrogen masses
-    mH2 = np.asarray([i.masses['H2'] for i in sim.galaxies])   # read in molecular hydrogen
-    sfr = np.asarray([i.sfr for i in sim.galaxies])   # read in instantaneous star formation rates
-    galpos = np.array([g.pos.d for g in sim.galaxies]) # the .d removes the units
-    caesar_id = np.array([i.GroupID for i in sim.galaxies]) # getting the Caesar ID for each galaxy
+        # Get galaxy info
+        gals = np.asarray([i.central for i in sim.galaxies])   # read in galaxies from caesar file
+        ms = np.asarray([i.masses['stellar'] for i in sim.galaxies])   # read in stellar masses of galaxies
+        mHI = np.asarray([i.masses['HI'] for i in sim.galaxies])   # read in neutral hydrogen masses
+        mH2 = np.asarray([i.masses['H2'] for i in sim.galaxies])   # read in molecular hydrogen
+        sfr = np.asarray([i.sfr for i in sim.galaxies])   # read in instantaneous star formation rates
+        galpos = np.array([g.pos.d for g in sim.galaxies]) # the .d removes the units
+        caesar_id = np.array([i.GroupID for i in sim.galaxies]) # getting the Caesar ID for each galaxy
 
-    ssfr_gal = sfr/ms
-    sfgals = 0
-    ssfr_cond = sfr_condition('end',thubble)
-    sfmass = []
-    for i in range(0, len(gals)):
-        if ssfr_gal[i] >= 10**ssfr_cond:
-            sfgals = sfgals + 1
-            sfmass.append(ms[i])
-    sfmass = np.asarray(sfmass)
-    d['sf_galaxies_mass'] = np.concatenate((d['sf_galaxies_mass'],sfmass))
-    print('Number of star forming galaxies in this snapshot: '+str(sfgals))
-    print('Median mass of star forming galaxies in this snapshot: '+str(np.median(d['sf_galaxies_mass'][s]))+' M*')
-    d['sf_galaxies_per_snap'][s] = sfgals
-    d['galaxies_per_snap'][s] = len(gals)
-    d['redshifts'][s] = redshift
-    d['t_hubble'][s] = thubble
-    if s==0:
-        d['boxsize_in_kpccm'] = sim.simulation.boxsize.to('kpccm')
-    for k in range(0, lengal):
+        ssfr_gal = sfr/ms
+        sfgals = 0
+        ssfr_cond = sfr_condition('end',thubble)
+        sfmass = []
+        for i in range(0, len(gals)):
+            if ssfr_gal[i] >= 10**ssfr_cond:
+                sfgals = sfgals + 1
+                sfmass.append(ms[i])
+        sfmass = np.asarray(sfmass)
+        d['sf_galaxies_mass'] = np.concatenate((d['sf_galaxies_mass'],sfmass))
+        print('Number of star forming galaxies in this snapshot: '+str(sfgals))
+        print('Median mass of star forming galaxies in this snapshot: '+str(np.median(d['sf_galaxies_mass'][s]))+' M*')
+        d['sf_galaxies_per_snap'][s] = sfgals
+        d['galaxies_per_snap'][s] = len(gals)
+        d['redshifts'][s] = redshift
+        d['t_hubble'][s] = thubble
         if s==0:
-            d['m_gal'+str(k)] = np.concatenate((d['m_gal'+str(k)], ms[k]), axis=None)
-            d['sfr_gal'+str(k)] = np.concatenate((d['sfr_gal'+str(k)],sfr[k]), axis=None)
-            frac = (mHI[k] + mH2[k])/ms[k]
-            h1 = mHI[k]/ms[k]
-            h2 = mH2[k]/ms[k]
-            d['fgas_gal'+str(k)] = np.concatenate((d['fgas_gal'+str(k)],frac), axis=None)
-            d['h1_gal'+str(k)] = np.concatenate((d['h1_gal'+str(k)],h1), axis=None)
-            d['h2_gal'+str(k)] = np.concatenate((d['h2_gal'+str(k)],h2), axis=None)
-            if (mH2[k])>0:
-                sfe = sfr[k]/(mH2[k])
-            else:
-                sfe = 0.0
-            d['sfe_gal'+str(k)] = np.concatenate((d['sfe_gal'+str(k)],sfe),axis=None)
-            d['t_gal'+str(k)] = np.concatenate((d['t_gal'+str(k)], thubble), axis=None)
-            d['z_gal'+str(k)] = np.concatenate((d['z_gal'+str(k)], redshift), axis=None)
-            d['gal_type'+str(k)] = np.concatenate((d['gal_type'+str(k)],gals[k]), axis=None)
-            d['caesar_id'+str(k)] = np.concatenate((d['caesar_id'+str(k)],caesar_id[k]), axis=None)
-            d['gal_pos'+str(k)] = np.array([galpos[k]])
+            d['boxsize_in_kpccm'] = sim.simulation.boxsize.to('kpccm')
+        for k in range(0, lengal):
+            if s==0:
+                d['m_gal'+str(k)] = np.concatenate((d['m_gal'+str(k)], ms[k]), axis=None)
+                d['sfr_gal'+str(k)] = np.concatenate((d['sfr_gal'+str(k)],sfr[k]), axis=None)
+                frac = (mHI[k] + mH2[k])/ms[k]
+                h1 = mHI[k]/ms[k]
+                h2 = mH2[k]/ms[k]
+                d['fgas_gal'+str(k)] = np.concatenate((d['fgas_gal'+str(k)],frac), axis=None)
+                d['h1_gal'+str(k)] = np.concatenate((d['h1_gal'+str(k)],h1), axis=None)
+                d['h2_gal'+str(k)] = np.concatenate((d['h2_gal'+str(k)],h2), axis=None)
+                if (mH2[k])>0:
+                    sfe = sfr[k]/(mH2[k])
+                else:
+                    sfe = 0.0
+                d['sfe_gal'+str(k)] = np.concatenate((d['sfe_gal'+str(k)],sfe),axis=None)
+                d['t_gal'+str(k)] = np.concatenate((d['t_gal'+str(k)], thubble), axis=None)
+                d['z_gal'+str(k)] = np.concatenate((d['z_gal'+str(k)], redshift), axis=None)
+                d['gal_type'+str(k)] = np.concatenate((d['gal_type'+str(k)],gals[k]), axis=None)
+                d['caesar_id'+str(k)] = np.concatenate((d['caesar_id'+str(k)],caesar_id[k]), axis=None)
+                d['gal_pos'+str(k)] = np.array([galpos[k]])
 
-        elif progenref_data[k][s-1] !=-1:
-            index = progenref_data[k][s-1]
-            d['m_gal'+str(k)] = np.concatenate((d['m_gal'+str(k)], ms[index]), axis=None)
-            d['sfr_gal'+str(k)] = np.concatenate((d['sfr_gal'+str(k)],sfr[index]), axis=None)
-            frac = (mHI[index] + mH2[index])/ms[index]
-            h1 = mHI[index]/ms[index]
-            h2 = mH2[index]/ms[index]
-            d['fgas_gal'+str(k)] = np.concatenate((d['fgas_gal'+str(k)],frac), axis=None)
-            d['h1_gal'+str(k)] = np.concatenate((d['h1_gal'+str(k)],h1), axis=None)
-            d['h2_gal'+str(k)] = np.concatenate((d['h2_gal'+str(k)],h2), axis=None)
-            if (mHI[index] + mH2[index])>0:
-                sfe = sfr[index]/(mHI[index] + mH2[index])
-            else:
-                sfe = 0.0
-            d['sfe_gal'+str(k)] = np.concatenate((d['sfe_gal'+str(k)],sfe),axis=None)
-            d['t_gal'+str(k)] = np.concatenate((d['t_gal'+str(k)], thubble), axis=None)
-            d['z_gal'+str(k)] = np.concatenate((d['z_gal'+str(k)], redshift), axis=None)
-            d['gal_type'+str(k)] = np.concatenate((d['gal_type'+str(k)],gals[index]), axis=None)
-            d['caesar_id'+str(k)] = np.concatenate((d['caesar_id'+str(k)],caesar_id[index]), axis=None)
-            #print(d['gal_pos'+str(k)])
-            #print([galpos[index]])
-            d['gal_pos'+str(k)] = np.concatenate((d['gal_pos'+str(k)],np.asarray([galpos[index]])), axis=0)
+            elif progenref_data[k][s-1] !=-1:
+                index = progenref_data[k][s-1]
+                d['m_gal'+str(k)] = np.concatenate((d['m_gal'+str(k)], ms[index]), axis=None)
+                d['sfr_gal'+str(k)] = np.concatenate((d['sfr_gal'+str(k)],sfr[index]), axis=None)
+                frac = (mHI[index] + mH2[index])/ms[index]
+                h1 = mHI[index]/ms[index]
+                h2 = mH2[index]/ms[index]
+                d['fgas_gal'+str(k)] = np.concatenate((d['fgas_gal'+str(k)],frac), axis=None)
+                d['h1_gal'+str(k)] = np.concatenate((d['h1_gal'+str(k)],h1), axis=None)
+                d['h2_gal'+str(k)] = np.concatenate((d['h2_gal'+str(k)],h2), axis=None)
+                if (mHI[index] + mH2[index])>0:
+                    sfe = sfr[index]/(mHI[index] + mH2[index])
+                else:
+                    sfe = 0.0
+                d['sfe_gal'+str(k)] = np.concatenate((d['sfe_gal'+str(k)],sfe),axis=None)
+                d['t_gal'+str(k)] = np.concatenate((d['t_gal'+str(k)], thubble), axis=None)
+                d['z_gal'+str(k)] = np.concatenate((d['z_gal'+str(k)], redshift), axis=None)
+                d['gal_type'+str(k)] = np.concatenate((d['gal_type'+str(k)],gals[index]), axis=None)
+                d['caesar_id'+str(k)] = np.concatenate((d['caesar_id'+str(k)],caesar_id[index]), axis=None)
+                #print(d['gal_pos'+str(k)])
+                #print([galpos[index]])
+                d['gal_pos'+str(k)] = np.concatenate((d['gal_pos'+str(k)],np.asarray([galpos[index]])), axis=0)
         #print(d['gal_pos'+str(k)])
 print('Data saved to dictionary.')
 output = open(results_folder+'progen_'+str(MODEL)+'.pkl','wb')
