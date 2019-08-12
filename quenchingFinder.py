f8be111ae17c8d39e53467f28d02f043c11f56f3
Curro Rodriguez Montero <s1650043@cplab223.ph.ed.ac.uk>
2019-08-12 13:42:56 +0100

    update

--- quenchingFinder.py
+++ quenchingFinder.py
@@ -192,7 +192,7 @@ def quench (galaxy,j,curr_state, sfr_condition, d_indx, interpolation=False):
     if t > 1.2*time_min:
         #It has passed enough time since the quench was detected, we can have rejuvenations
 
-        if ssfr_gal > 10**sfr_condition('end', galaxy, j, d_indx):
+        if ssfr_gal > 10**current_lssfr:
             #We have found a sign change
             if reju_condition(galaxy, j, d_indx):
                 if interpolation:
@@ -207,7 +207,7 @@ def quench (galaxy,j,curr_state, sfr_condition, d_indx, interpolation=False):
             new_state = curr_state
     else:
         #Now if there is a rejuvenation, the quench should be discarded.
-        if ssfr_gal > 10**current_lssfr:
+        if ssfr_gal > 10**sfr_condition('end', galaxy, j, d_indx):
             #We have found a sign change. Rollback the changes done to the lists.
             del galaxy.quenching[-1]
 
